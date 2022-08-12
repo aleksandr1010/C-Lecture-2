550da4d20fb9de26b1c6caf372bbbacfb21b8c48
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-12 15:10:30 +0300

    Task_03

---
+++ Exemp03_IntroArrey/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//            0    1  2   3   4    5   6   7   8
+int[] arrey ={13, 21, 63, 34, 51, 16, 87, 128, 99};
+arrey[0] = 25;
+
+int max = Max(
+    Max(arrey[0], arrey[1], arrey[2]),
+    Max(arrey[3], arrey[4], arrey[5]), 
+    Max(arrey[6], arrey[7], arrey[8])
+);
+Console.WriteLine(max);
